122e16c0946a2193eb4d1d6942313e2007d1f82f
alexa griffin <alexa.griffin42@gmail.com>
2018-09-29 14:13:06 -0400

    various recipe functions

--- zenith-world_0.0.1/functions/item.lua
+++ zenith-world_0.0.1/functions/item.lua
@@ -1,6 +1,17 @@
 if not zen.lib.item then zen.lib.item = {} end
 
-
+-- duplicates item from raw
 function zen.lib.item.duplicateFromRaw(name, overrides)
   return zen.lib.duplicateFromRaw("item", name, overrides)
+end
+
+-- is an item a fluid
+function zen.lib.item.isFluid(name)
+  if data.raw.fluid[name] then return true end
+  return false
+end
+
+zen.lib.item.exists(name)
+  if data.raw.fluid[name] or data.raw.item[name] then return true end
+  return false
 end
--- zenith-world_0.0.1/functions/recipe.lua
+++ zenith-world_0.0.1/functions/recipe.lua
@@ -1,10 +1,13 @@
 if not zen.lib.recipe then zen.lib.recipe = {} end
 
-
-function zen.lib.recipe.duplicateFromRaw(name, overrides)
+-- duplicates recipe from raw
+function zen.lib.recipe.duplicateFromRaw(name, overrides, prod)
+  prod = prod or false
+  if prod then zen.lib.recipe.addProdLim(name) end
   return zen.lib.duplicateFromRaw("recipe", name, overrides)
 end
 
+-- adds productivity limitations
 function zen.lib.recipe.addProdLim(name)
   if data.raw.recipe[name] then
     for i, module in pairs(data.raw.module) do
@@ -14,5 +17,34 @@ function zen.lib.recipe.addProdLim(name)
     end
   else
     log("recipe: \"" .. name .. "\" does not exist")
+  end
+end
+
+-- removes productivity limitations
+function zen.lib.recipe.removeProdLim(name)
+  if data.raw.recipe[name] then
+    for i, module in pairs(data.raw.module) do
+      if module.limitation and module.effect.productivity then
+        table.remove(module.limitation, name)
+      end
+    end
+  else
+    log("recipe: \"" .. name .. "\" does not exist")
+  end
+end
+
+-- adds an ingredient to a recipe
+function.zen.lib.recipe.addIngredient(recipe, ingredient, amount)
+  amount = amount or 1
+  if data.raw.recipe[recipe] and zen.lib.item.exists(ingredient) then
+    table.insert(data.raw.recipe[recipe].ingredient, {
+      ingredient, amount
+    })
+  else
+    if not data.raw.recipe[recipe] then
+      log("recipe: \"" .. recipe .. "\" does not exist")
+    else
+      log("item/fluid: \"" .. ingredient .. "\" does not exist")
+    end
   end
 end
