56a5f992a806436a688a0d231dc14c6e13dc21ff
alexa griffin <alexa.griffin42@gmail.com>
2018-09-28 20:34:03 -0400

    added debug and beta settings

--- zenith-world_0.0.1/data.lua
+++ zenith-world_0.0.1/data.lua
@@ -1,1 +1,10 @@
+if not zen then zen = {} end
+if not zen.world then zen.world = {} end
+if not zen.lib then zen.lib = {} end
 
+zen.world.debug = settings.startup["zen-debug"].value
+zen.world.beta = settings.startup["zen-beta"].value
+
+
+if zen.world.
+require("test")
---
+++ zenith-world_0.0.1/settings.lua
@@ -0,0 +1,14 @@
+data:extend({
+  {
+    type = "bool-setting",
+    name = "zenith-beta",
+    setting_type = "runtime-global",
+    default_value = false
+  },
+  {
+    type = "bool-setting",
+    name = "zenith-debug",
+    setting_type = "runtime-global",
+    default_value = false
+  }
+})
