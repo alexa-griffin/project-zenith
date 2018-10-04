a3fb2bf7339f15e48346e3d3cc24f0008d468649
alexa griffin <alexa.griffin42@gmail.com>
2018-10-04 11:57:15 -0400

    added colors

--- zenith-chemistry_0.0.1/prototypes/fluid.lua
+++ zenith-chemistry_0.0.1/prototypes/fluid.lua
@@ -1,10 +1,14 @@
 data:extend({
   zen.lib.duplicateFromRaw("fluid", "heavy-oil", {
     name = "ammonia",
-    icon = "__zenith-chemistry__/graphics/icons/ammonia.png"
+    icon = "__zenith-chemistry__/graphics/icons/ammonia.png",
+    base_color = {r=0.4, g=0.4, b=0.4},
+    flow_color = {r=0.8, g=0.8, b=0.8},
   }),
   zen.lib.duplicateFromRaw("fluid", "heavy-oil", {
     name = "cyanide",
-    icon = "__zenith-chemistry__/graphics/icons/cyanide.png"
+    icon = "__zenith-chemistry__/graphics/icons/cyanide.png",
+    base_color = {r=0.8, g=0.4, b=0.4},
+    flow_color = {r=0.8, g=0.8, b=0.8},
   }),
 })
