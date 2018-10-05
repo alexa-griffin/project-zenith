2355ca955f7667a186a3a231a6914977e29b6093
alexa griffin <alexa.griffin42@gmail.com>
2018-10-05 14:40:35 -0400

    fixed load order

--- zenith-chemistry_0.0.1/data-updates.lua
+++ zenith-chemistry_0.0.1/data-updates.lua
@@ -1,3 +1,4 @@
+data.raw.fluid["petroleum-gas"].icon = "__zenith-chemistry__/graphics/icons/petroleum-gas.png"
 
 zen.lib.recipe.replaceIngredient("plastic-bar", "petroleum", "raw-polyethylene")
 zen.lib.recipe.replaceIngredient("sulfur", "water", "ethylene", 20)
@@ -5,8 +6,3 @@ zen.lib.recipe.addIngredient("lubricant", "stone", 2)
 
 zen.lib.tech.addPack("sulfur-processing", "science-pack-3")
 zen.lib.tech.addPrereq("sulfur-processing", "advanced-oil-processing")
-
-require("prototypes.item")
-require("prototypes.technology")
-require("prototypes.recipe")
-require("prototypes.fluid")
--- zenith-chemistry_0.0.1/data.lua
+++ zenith-chemistry_0.0.1/data.lua
@@ -2,3 +2,8 @@ if not zen then zen = {} end
 if not zen.chemistry then zen.chemistry = {} end
 
 data.raw.fluid["petroleum-gas"].icon = "__zenith-chemistry__/graphics/icons/petroleum-gas.png"
+
+require("prototypes.fluid")
+require("prototypes.item")
+require("prototypes.technology")
+require("prototypes.recipe")
