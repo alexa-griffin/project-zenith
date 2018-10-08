a20d1bb3e4c2c8e99d11393bdfdfa0db4ffa89ca
alexa griffin <alexa.griffin42@gmail.com>
2018-10-08 16:25:03 -0400

    import all the bio crap that i did a while ago

--- zenith-bio_0.0.1/data.lua
+++ zenith-bio_0.0.1/data.lua
@@ -1,1 +1,2 @@
-
+if not zen then zen = {} end
+if not zen.bio then zen.bio = {} end
---
+++ zenith-bio_0.0.1/prototypes/arboretum.lua
@@ -0,0 +1,158 @@
+data:extend({
+  {
+    type = "assembling-machine",
+    name = "arboretum",
+    icon = "__zenith-bio__/graphics/icons/arboretum.png",
+    icon_size = 32,
+    flags = {"placeable-neutral", "placeable-player", "player-creation"},
+    minable = {hardness = 0.2, mining_time = 0.5, result = "arboretum"},
+    max_health = 350,
+    corpse = "big-remnants",
+    dying_explosion = "medium-explosion",
+    alert_icon_shift = util.by_pixel(-3, -12),
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 70
+      }
+    },
+    fluid_boxes = {
+      {
+        production_type = "input",
+        pipe_picture = assembler3pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {0, -2} }}
+      },
+      off_when_no_fluid_recipe = true
+    },
+    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+    fast_replaceable_group = "arboretum",
+    animation =
+    {
+      layers =
+      {
+        {
+          filename = "__zenith-bio__/graphics/entity/arboretum/arboretum.png",
+          width = 98,
+          height = 87,
+          frame_count = 33,
+          line_length = 11,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(0, 1.5),
+          hr_version =
+          {
+            filename = "__zenith-bio__/graphics/entity/arboretum/hr-arboretum.png",
+            width = 194,
+            height = 174,
+            frame_count = 33,
+            line_length = 11,
+            animation_speed = 1 / 3,
+            shift = util.by_pixel(0, 1.5),
+            scale = 0.5
+          }
+        },
+        {
+          filename = "__base__/graphics/entity/lab/lab-integration.png",
+          width = 122,
+          height = 81,
+          frame_count = 1,
+          line_length = 1,
+          repeat_count = 33,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(0, 15.5),
+          hr_version =
+          {
+            filename = "__base__/graphics/entity/lab/hr-lab-integration.png",
+            width = 242,
+            height = 162,
+            frame_count = 1,
+            line_length = 1,
+            repeat_count = 33,
+            animation_speed = 1 / 3,
+            shift = util.by_pixel(0, 15.5),
+            scale = 0.5
+          }
+        },
+        {
+          filename = "__base__/graphics/entity/lab/lab-shadow.png",
+          width = 122,
+          height = 68,
+          frame_count = 1,
+          line_length = 1,
+          repeat_count = 33,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(13, 11),
+          draw_as_shadow = true,
+          hr_version =
+          {
+            filename = "__base__/graphics/entity/lab/hr-lab-shadow.png",
+            width = 242,
+            height = 136,
+            frame_count = 1,
+            line_length = 1,
+            repeat_count = 33,
+            animation_speed = 1 / 3,
+            shift = util.by_pixel(13, 11),
+            scale = 0.5,
+            draw_as_shadow = true
+          }
+        }
+      }
+    },
+    working_sound =
+    {
+      sound =
+      {
+        filename = "__zenith-bio__/sound/scream.ogg",
+        volume = 0.4
+      },
+      max_sounds_per_type = 3
+    },
+    open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
+    close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    crafting_categories = { "arboretuming" },
+    crafting_speed = 1,
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      emissions = 0.04 / 2.5
+    },
+    energy_usage = "150kW",
+    ingredient_count = 4,
+    module_specification =
+    {
+      module_slots = 2
+    },
+    allowed_effects = {"consumption", "speed", "productivity", "pollution"}
+  },
+  {
+    type = "item",
+    name = "arboretum",
+    icon = "__zenith-bio__/graphics/icons/arboretum.png",
+    icon_size = 32,
+    flags = {"goes-to-quickbar"},
+    subgroup = "production-machine",
+    order = "h",
+    place_result = "arboretum",
+    stack_size = 10
+  },
+  {
+    type = "recipe",
+    name = "arboretum",
+    energy_required = 2,
+    enabled = false,
+    ingredients =
+    {
+      { "iron-plate", 5 },
+      { "electronic-circuit", 5 },
+      { "stone", 5 },
+    },
+    result = "arboretum"
+  },
+})
