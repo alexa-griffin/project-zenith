93c24cabc527ac0fa5091abf501da257952acb6d
alexa griffin <alexa.griffin42@gmail.com>
2018-10-16 16:55:48 -0400

    I

--- module-zenith_0.0.1/data.lua
+++ module-zenith_0.0.1/data.lua
@@ -8,9 +8,14 @@ zen.settings.large_beacon = settings.startup["zenith-large-beacon"].value
 
 require("prototypes.item-groups")
 require("prototypes.module-function")
+require("prototypes.categories")
 require("prototypes.item")
 require("prototypes.recipe")
 require("prototypes.technology")
+
 require("prototypes.speed-module")
 require("prototypes.effectivity-module")
 require("prototypes.productivity-module")
+require("prototypes.pure-speed-module")
+require("prototypes.pure-productivity-module")
+require("prototypes.god-module")
