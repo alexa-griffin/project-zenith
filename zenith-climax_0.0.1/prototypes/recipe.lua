427c9e91054087cf8c037fe5fccdd79909619079
alexa griffin <alexa.griffin42@gmail.com>
2018-10-20 16:58:11 -0400

    heck

--- zenith-climax_0.0.1/prototypes/recipe.lua
+++ zenith-climax_0.0.1/prototypes/recipe.lua
@@ -8,7 +8,80 @@ data:extend({
       { "superluminal-thruster", 1 },
       { "superluminal-brain", 1 },
       { "superluminal-tachyon-generator", 1 },
-    }
-    result = "superluminal-rocket-part"
+    },
+    result = "superluminal-rocket-part",
+  }),
+  zen.lib.recipe.duplicateFromRaw("satellite", {
+    name = "superluminal-power-supply",
+    ingredients = {
+      { "fusion-reactor", 500 },
+      { "nuclear-reactor", 100 },
+      { "plutonium-fuel-cell", 500 },
+      { "uranium-fuel-cell", 500 },
+      { "thorium-fuel-cell", 500 },
+      { "pionium", 500 },
+    },
+    result = "superluminal-power-supply",
+  }),
+  zen.lib.recipe.duplicateFromRaw("satellite", {
+    name = "superluminal-brain",
+    ingredients = {
+      { "rocket-control-unit", 500 },
+      { "god-module-7", 100 },
+      { "pure-speed-module-7", 100 },
+      { "pure-productivity-module-7", 100 },
+      { "bio-computer", 500 },
+    },
+    result = "superluminal-brain",
+  }),
+  zen.lib.recipe.duplicateFromRaw("satellite", {
+    name = "superluminal-data-transfer",
+    ingredients = {
+      { "rocket-control-unit", 100 },
+      { "speed-module-7", 100 },
+      { "productivity-module-7", 100 },
+      { "effectivity-module-7", 100 },
+      { "bio-computer", 100 },
+      { "radar", 100 },
+    },
+    result = "superluminal-data-transfer",
+  }),
+  zen.lib.recipe.duplicateFromRaw("satellite", {
+    name = "superluminal-drive",
+    ingredients = {
+      { "rocket-fuel", 500 },
+      { "fusion-reactor", 500 },
+      { "portable-fusion-reactor", 500 },
+    },
+    result = "superluminal-drive",
+  }),
+  zen.lib.recipe.duplicateFromRaw("satellite", {
+    name = "superluminal-structure",
+    ingredients = {
+      { "low-density-structure", 1000 },
+      { "nuclear-polymer", 1000 },
+      { "scandium-plate", 1000 },
+      { "energy-shield-mk2-equipment", 1000 },
+    },
+    result = "superluminal-structure",
+  }),
+  zen.lib.recipe.duplicateFromRaw("satellite", {
+    name = "superluminal-tachyon-generator",
+    ingredients = {
+      { "discharge-defense-equipment", 1000 },
+      { "nuclear-polymer", 1000 },
+      { "pionium", 1000 },
+    },
+    result = "superluminal-tachyon-generator",
+  }),
+  zen.lib.recipe.duplicateFromRaw("satellite", {
+    name = "superluminal-thruster",
+    ingredients = {
+      { "discharge-defense-equipment", 1000 },
+      { "plutonium-fuel-cell", 500 },
+      { "uranium-fuel-cell", 500 },
+      { "thorium-fuel-cell", 500 },
+    },
+    result = "superluminal-thruster",
   }),
 })
