f27af339d06872ad5631a08d6efd412a680342e7
alexa griffin <alexa.griffin42@gmail.com>
2018-10-20 19:44:41 -0400

    heck

--- zenith-climax_0.0.1/prototypes/item.lua
+++ zenith-climax_0.0.1/prototypes/item.lua
@@ -1,35 +1,35 @@
 data:extend({
-  zen.lib.item.duplicateFromRaw("uranium-fuel-cell", {
+  zen.lib.item.duplicateFromRaw("low-density-structure", {
     name = "superluminal-data-transfer-computer",
     icon = "__zenith-climax__/graphics/icons/superluminal-data-transfer-computer.png",
     stack_size = 1,
   }),
-  zen.lib.item.duplicateFromRaw("uranium-fuel-cell", {
+  zen.lib.item.duplicateFromRaw("low-density-structure", {
     name = "superluminal-brain",
     icon = "__zenith-climax__/graphics/icons/superluminal-brain.png",
     stack_size = 1,
   }),
-  zen.lib.item.duplicateFromRaw("uranium-fuel-cell", {
+  zen.lib.item.duplicateFromRaw("low-density-structure", {
     name = "superluminal-power-supply",
     icon = "__zenith-climax__/graphics/icons/superluminal-power-supply.png",
     stack_size = 1,
   }),
-  zen.lib.item.duplicateFromRaw("uranium-fuel-cell", {
+  zen.lib.item.duplicateFromRaw("low-density-structure", {
     name = "superluminal-drive",
     icon = "__zenith-climax__/graphics/icons/superluminal-drive.png",
     stack_size = 1,
   }),
-  zen.lib.item.duplicateFromRaw("uranium-fuel-cell", {
+  zen.lib.item.duplicateFromRaw("low-density-structure", {
     name = "superluminal-tachyon-generator",
     icon = "__zenith-climax__/graphics/icons/superluminal-tachyon-generator.png",
     stack_size = 1,
   }),
-  zen.lib.item.duplicateFromRaw("uranium-fuel-cell", {
+  zen.lib.item.duplicateFromRaw("low-density-structure", {
     name = "superluminal-structure",
     icon = "__zenith-climax__/graphics/icons/superluminal-structure.png",
     stack_size = 1,
   }),
-  zen.lib.item.duplicateFromRaw("uranium-fuel-cell", {
+  zen.lib.item.duplicateFromRaw("low-density-structure", {
     name = "superluminal-thruster",
     icon = "__zenith-climax__/graphics/icons/superluminal-thruster.png",
     stack_size = 1,
@@ -41,5 +41,7 @@ data:extend({
   zen.lib.item.duplicateFromRaw("rocket-silo", {
     name = "superluminal-rocket-silo",
     place_result = "superluminal-rocket-silo",
+    icon = "__zenith-climax__/graphics/icons/superluminal-rocket-silo.png",
+
   }),
 })
