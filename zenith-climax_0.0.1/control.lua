5e2d34da90c120809ae44eeb8f58f60ab2eea112
alexa griffin <alexa.griffin42@gmail.com>
2018-10-20 23:32:35 -0400

    gah

--- zenith-climax_0.0.1/control.lua
+++ zenith-climax_0.0.1/control.lua
@@ -1,5 +1,12 @@
-local function disable_no_satellite_launch_message()
-  if remote.interfaces["silo-script"] then
-    remote.call("silo-script", "set_show_launched_without_satellite", false)
-  end
-end
+script.on_event(defines.events.on_rocket_launched, function(event)
+  local status, err = pcall(function()
+  	remote.call("silo_script","set_show_launched_without_satellite", false)
+  	remote.call("silo_script","set_finish_on_launch", false)
+  end)
+  game.set_game_state({
+    game_finished = false, 
+    player_won = false, 
+    can_continue = true
+  })
+
+end)
