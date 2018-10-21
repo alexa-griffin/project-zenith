20f44c2979fe8df9f8a7e568386d5c7e5d026f28
alexa griffin <alexa.griffin42@gmail.com>
2018-10-20 23:05:47 -0400

    h?e?c?k?

--- zenith-climax_0.0.1/control.lua
+++ zenith-climax_0.0.1/control.lua
@@ -3,11 +3,3 @@ local function disable_no_satellite_launch_message()
     remote.call("silo-script", "set_show_launched_without_satellite", false)
   end
 end
-
--- function on_rocket_launch(event)
---   if event.rocket_silo.name == "superluminal-rocket-silo" then
---
---
---   end
---
--- end
