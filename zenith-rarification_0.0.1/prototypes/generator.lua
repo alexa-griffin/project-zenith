affdd9469a203fa78022c7ebed50c3a619f896b0
alexa griffin <alexa.griffin42@gmail.com>
2018-10-25 13:03:11 -0400

    ok i think this is good

--- zenith-rarification_0.0.1/prototypes/generator.lua
+++ zenith-rarification_0.0.1/prototypes/generator.lua
@@ -1,4 +1,3 @@
-
 function generateOreItems(settings)
   data:extend({
     zen.lib.item.duplicateFromRaw("iron-ore", {
@@ -32,6 +31,78 @@ function generateOreItems(settings)
     zen.lib.item.duplicateFromRaw("iron-ore", {
       name = "pure-" .. settings.name,
       icon = "__zenith-rarification__/graphics/icons/pure-" .. settings.name .. ".png",
+    }),
+  })
+end
+
+function generateOreRecipe(settings)
+  data:extend({
+    zen.lib.recipe.duplicateFromRaw("iron-ore", {
+      name = "raw-" .. settings.name .. "-ore",
+      category = "ore-processing",
+      ingredients = settings.initial_ingredients,
+      result = "raw-" .. settings.name .. "-ore",
+      result_count = settings.initial_result_count
+    }),
+    zen.lib.recipe.duplicateFromRaw("iron-ore", {
+      name = settings.name .. "-dust",
+      category = "ore-processing",
+      ingredients = {
+        { "raw-" .. settings.name .. "-ore", 1 }
+      },
+      result = settings.name .. "-dust",
+      result_count = 3
+    }),
+    zen.lib.recipe.duplicateFromRaw("iron-ore", {
+      name = settings.name .. "-slurry",
+      category = "ore-processing",
+      ingredients = {
+        { settings.name .. "-dust", 2 },
+        { type = "fluid", name = "water", amount = 10 },
+      },
+      results = {
+        { type = "fluid, "name = settings.name .. "-slurry", amount = 10 },
+      }
+    }),
+    zen.lib.recipe.duplicateFromRaw("iron-ore", {
+      name = settings.name .. "-nugget",
+      category = "ore-processing",
+      ingredients = {
+        { type = "fluid", name = settings.name .. "-slurry", amount = 10 },
+      },
+      main_product = settings.name .. "-nugget",
+      results = {
+        settings.name .. "-nugget",
+        { type = "fluid", name = settings.wastewater, amount = 10 },
+      }
+    }),
+    zen.lib.recipe.duplicateFromRaw("iron-ore", {
+      name = settings.name .. "-crystals",
+      category = "ore-processing",
+      ingredients = {
+        { settings.name .. "-nugget", 1 },
+        { settings.name .. "-dust", 1 },
+        { type = "fluid", name = settings.extractor_additive, amount = 10 },
+      },
+      main_product = settings.name .. "-crystals",
+      results = {
+        { settings.name .. "-crystals", 10 },
+        { type = "fluid", name = settings.wastewater, amount = 10 },
+      }
+    }),
+    zen.lib.recipe.duplicateFromRaw("iron-ore", {
+      name = settings.name .. "-essence",
+      category = "ore-processing",
+      ingredients = {
+        { settings.name .. "-crystals", 1 },
+        { settings.essence_additive, 2 },
+        { type = "fluid", name = settings.extractor_additive, amount = 10 },
+      },
+      main_product = settings.name .. "-essence",
+      results = {
+        settings.name .. "-essence",
+        { type = "fluid", name = settings.wastewater, amount = 10 },
+      }
     }),
   })
 end
---
+++ zenith-rarification_0.0.1/prototypes/ore.lua
@@ -0,0 +1,13 @@
+generateOreItems({
+  name = "iron",
+  slurry = {
+    base_color = "#51898c",
+    flow_color = "#323535",
+  },
+  essence = {
+    base_color = "#547d8c",
+    flow_color = "#73b0c6",
+  },
+})
+
+generateOreRecipe()
